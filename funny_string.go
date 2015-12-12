a7502cfe95a769a69bbcaf53b6c282e452dca2cd
Aman Mangal <mangalaman93@gmail.com>
2015-12-11 21:13:33 -0500

    Correct indentation of funny_string.go

--- funny_string.go
+++ funny_string.go
@@ -1,36 +1,37 @@
 package main
+
 import "fmt"
 
 func abs(n int) int {
-    if n < 0 {
-        n = -n
-    }
-    
-    return n
+	if n < 0 {
+		n = -n
+	}
+
+	return n
 }
 
 func main() {
-    var N int
-    fmt.Scan(&N)
-    for j:=0; j<N; j++ {
-        var s string
-        fmt.Scan(&s)
-        
-        flag := true
-        l := len(s)
-        for i:=1; i<l; i++ {
-            x := abs(int(s[i])-int(s[i-1]))
-            y := abs(int(s[l-1-i]) - int(s[l-i]))
-            if x != y {
-                flag = false
-                break
-            }
-        }
-        
-        if flag {
-            fmt.Println("Funny")
-        } else {
-            fmt.Println("Not Funny")
-        }
-    }
+	var N int
+	fmt.Scan(&N)
+	for j := 0; j < N; j++ {
+		var s string
+		fmt.Scan(&s)
+
+		flag := true
+		l := len(s)
+		for i := 1; i < l; i++ {
+			x := abs(int(s[i]) - int(s[i-1]))
+			y := abs(int(s[l-1-i]) - int(s[l-i]))
+			if x != y {
+				flag = false
+				break
+			}
+		}
+
+		if flag {
+			fmt.Println("Funny")
+		} else {
+			fmt.Println("Not Funny")
+		}
+	}
 }
